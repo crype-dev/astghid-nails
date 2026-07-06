c66ac74a0e6baa09527e14950f1449840231970d
crype-dev <marcoyeranosyan1@gmail.com>
2026-07-06 16:07:47 +0200

    fix: remplace proxy domaine par redirection next

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,21 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: process.cwd(),
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.astghidnails.com",
+          },
+        ],
+        destination: "https://astghidnails.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
